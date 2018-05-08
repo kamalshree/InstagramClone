c0a11eafb83ef3ac2f9cd6b9b15e39b9129ddd8d
codesqills <codesqills@gmail.com>
2018-05-08 15:57:31 -0400

    Created the profile screen

--- app/src/main/java/com/android/instagramclone/Profile/ProfileActivity.java
+++ app/src/main/java/com/android/instagramclone/Profile/ProfileActivity.java
@@ -1,13 +1,16 @@
 package com.android.instagramclone.Profile;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.android.instagramclone.Home.HomeActivity;
 import com.android.instagramclone.R;
 import com.android.instagramclone.Utils.BottomNavigationHelper;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
@@ -24,10 +27,11 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_profile);
         activity_number=getIntent().getIntExtra("activity_number",activity_number);
         Log.d(TAG, "onCreate: Profile ativity started");
-        setupBottomNavigationView();
+        //setupBottomNavigationView();
+        setupProfileToolbar();
     }
 
 
@@ -42,5 +46,38 @@ public class ProfileActivity extends AppCompatActivity {
         Menu menu=bottomNavigationViewEx.getMenu();
         MenuItem menuItem=menu.getItem(activity_number);
         menuItem.setChecked(true);
+    }
+
+    /**
+     * setup Toolbar
+     *
+     */
+    private void setupProfileToolbar(){
+        Toolbar toolbar=(Toolbar)findViewById(R.id.profileToolBar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Log.d(TAG, "onMenuItemClick: clicked menu item");
+                switch (item.getItemId()) {
+                    case R.id.profileMenu:
+                        Log.d(TAG, "onMenuItemClick: navigating to settings page");
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+
+    /**
+     * inflate the menu
+     */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_profile,menu);
+        return true;
     }
 }
