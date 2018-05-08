a1b5343a84b04b08ff35b4c902fb0737a3377c31
codesqills <codesqills@gmail.com>
2018-05-07 20:02:36 -0400

    Fragment and tag implementation

--- app/src/main/java/com/android/instagramclone/Home/HomeActivity.java
+++ app/src/main/java/com/android/instagramclone/Home/HomeActivity.java
@@ -1,11 +1,14 @@
 package com.android.instagramclone.Home;
 
 import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TableLayout;
 
 import com.android.instagramclone.R;
 import com.android.instagramclone.Utils.BottomNavigationHelper;
@@ -23,6 +26,7 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         activity_number=getIntent().getIntExtra("activity_number",activity_number);
         setupBottomNavigationView();
+        setupViewPager();
     }
 
     /**
@@ -36,5 +40,25 @@ public class HomeActivity extends AppCompatActivity {
         Menu menu=bottomNavigationViewEx.getMenu();
         MenuItem menuItem=menu.getItem(activity_number);
         menuItem.setChecked(true);
+    }
+
+    /**
+     * Top Tabs in Home Fragment
+     */
+    private void setupViewPager(){
+        SectionsPagerAdapter sectionsPagerAdapter=new SectionsPagerAdapter(getSupportFragmentManager());
+        sectionsPagerAdapter.addFragments(new CameraFragment());
+        sectionsPagerAdapter.addFragments(new HomeFragment());
+        sectionsPagerAdapter.addFragments(new MessageFragment());
+
+        ViewPager viewPager=(ViewPager)findViewById(R.id.container);
+        viewPager.setAdapter(sectionsPagerAdapter);
+
+        TabLayout tabLayout=(TabLayout)findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_logo);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_arrow);
     }
 }
---
+++ app/src/main/java/com/android/instagramclone/Home/SectionsPagerAdapter.java
@@ -0,0 +1,35 @@
+package com.android.instagramclone.Home;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kamalshree on 5/7/2018.
+ */
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragments(Fragment fragments) {
+        mFragmentList.add(fragments);
+    }
+}
